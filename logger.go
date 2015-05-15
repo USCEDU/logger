c8d2537ab25f5a2ff6883a20692f5e74afe1214d
zhangzhe <zhangzhe@unisound.com>
2015-05-15 12:38:13 +0800

    1.Add detail information about the position where log occur

--- logger.go
+++ logger.go
@@ -1,31 +1,32 @@
 package logger
 
-import(
+import (
+	"fmt"
 	"log"
 	"net"
-	"fmt"
+	"runtime"
 	"time"
 )
 
-type Logger interface{
+type Logger interface {
 	Log(level string, fmt string, v ...interface{})
 	LogE(fmt string, v ...interface{})
 	LogI(fmt string, v ...interface{})
 	LogW(fmt string, v ...interface{})
 }
 
-type logger struct{
-	addr *net.UDPAddr
-	conn *net.UDPConn
+type logger struct {
+	addr    *net.UDPAddr
+	conn    *net.UDPConn
 	console bool
-	tag string
+	tag     string
 }
 
-func NewLogger(tag string, addr string, console bool) Logger{
+func NewLogger(tag string, addr string, console bool) Logger {
 	ret := new(logger)
 	ret.console = console
 	ret.tag = tag
-	if addr == ""{
+	if addr == "" {
 		return ret
 	}
 	serverAddr, err := net.ResolveUDPAddr("udp", addr)
@@ -42,8 +43,8 @@ func NewLogger(tag string, addr string, console bool) Logger{
 	return ret
 }
 
-func (this logger) Log(level string, f string, v ...interface{}){
-	if !this.console && this.addr == nil{
+func (this logger) Log(level string, f string, v ...interface{}) {
+	if !this.console && this.addr == nil {
 		return
 	}
 	now := time.Now()
@@ -53,27 +54,33 @@ func (this logger) Log(level string, f string, v ...interface{}){
 		this.tag,
 		now.Format("2006-01-02 15:04:05.000"),
 		level)
+
+	pc, file, line, _ := runtime.Caller(1)
+	funcInfo := runtime.FuncForPC(pc)
+	logPos := fmt.Sprintf("FUNC:%s@LINE:%d@FILE:%s", funcInfo.Name(), line, file)
+
 	if this.console {
-		log.Println(head, msg)
+		log.Println(head, logPos, msg)
 	}
+
 	if this.addr != nil {
-		go func(){
+		go func() {
 			data := []byte(head)
-			data = append(data, []byte(msg)...)
+			data = append(data, []byte(logPos+msg)...)
+			//data = append(data, )
 			this.conn.Write(data)
 		}()
 	}
 }
 
-func (this logger) LogE(fmt string, v ...interface{}){
+func (this logger) LogE(fmt string, v ...interface{}) {
 	this.Log("ERROR", fmt, v...)
 }
 
-func (this logger) LogW(fmt string, v ...interface{}){
+func (this logger) LogW(fmt string, v ...interface{}) {
 	this.Log("WARN", fmt, v...)
 }
 
-func (this logger) LogI(fmt string, v ...interface{}){
+func (this logger) LogI(fmt string, v ...interface{}) {
 	this.Log("INFO", fmt, v...)
 }
-
